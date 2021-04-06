7cf75dbf6aa9ca037070d1d6ca931e171d0a6e18
profhdeivisson <profhdeivisson@yahoo.com>
2021-04-05 22:43:09 -0300

    Cronometro Iniciado

---
+++ cronometroJS/script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var h = 0
+var m = 0
+var s = 0
+
+var tempo = 1000 // Equivale a 1 segundo
+var cron
+
+function start(){
+    cron = setInterval(() => {timer()}, tempo)
+}
+function pause(){
+    clearInterval(cron)
+}
+function parar(){
+    clearInterval(cron)
+    h = 0
+    m = 0
+    s = 0
+    document.getElementById('cont').innerText = '00:00:00'
+}
+function timer(){
+    s++
+    if(s == 60){
+        s = 0
+        m++
+        if(m == 60){
+            m = 0
+            h++
+        }
+    }
+
+    var format = (h < 10 ? '0'+h : h) + ':' + (m < 10 ? '0'+m : m) + ':' + (s < 10 ? '0'+s : s)
+    document.getElementById('cont').innerText = format
+}
